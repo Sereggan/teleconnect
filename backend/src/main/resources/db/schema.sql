568b1009cfd317f3076465116c53c30f859d9306
sereggan <sergei.ni@yandex.ru>
2024-09-15 01:41:19 +0200

    Add unit tests for TariffService

--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -4,20 +4,10 @@ DROP TABLE IF EXISTS token_blacklist CASCADE;
 DROP TABLE IF EXISTS tariff_adjustment CASCADE;
 
 CREATE TABLE IF NOT EXISTS tariff (
-                                      id
-                                      SERIAL
-                                      PRIMARY
-                                      KEY,
-                                      name
-                                      VARCHAR
-(
-                                      100
-) NOT NULL UNIQUE,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
     price NUMERIC(10, 2) NOT NULL,
-    description VARCHAR
-(
-    255
-) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     is_active BOOLEAN DEFAULT FALSE,
     data_limit INTEGER DEFAULT 0,
     call_minutes INTEGER DEFAULT 0,
@@ -25,15 +15,8 @@ CREATE TABLE IF NOT EXISTS tariff (
     );
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id
-                                     SERIAL
-                                     PRIMARY
-                                     KEY,
-                                     phone_number
-                                     VARCHAR
-(
-                                     15
-) NOT NULL UNIQUE,
+    id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(15) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     name VARCHAR(30) NOT NULL,
@@ -41,73 +24,26 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(50) NOT NULL,
     tariff_id INTEGER,
     birth_date DATE,
-    CONSTRAINT fk_tariff FOREIGN KEY
-(
-    tariff_id
-) REFERENCES tariff
-(
-    id
-)
+    CONSTRAINT fk_tariff FOREIGN KEY (tariff_id) REFERENCES tariff(id)
     );
 
-CREATE TABLE IF NOT EXISTS tariff_adjustment
-(
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    adjusted_data_limit
-    INTEGER,
-    adjusted_call_minutes
-    INTEGER,
-    adjusted_sms_limit
-    INTEGER,
-    discount_percentage
-    NUMERIC
-(
-    5,
-    2
-),
+CREATE TABLE IF NOT EXISTS tariff_adjustment (
+    id SERIAL PRIMARY KEY,
+    adjusted_data_limit INTEGER,
+    adjusted_call_minutes INTEGER,
+    adjusted_sms_limit INTEGER,
+    discount_percentage NUMERIC(5, 2),
     tariff_id INTEGER NOT NULL UNIQUE,
     user_id INTEGER NOT NULL UNIQUE,
-    CONSTRAINT fk_tariff FOREIGN KEY
-(
-    tariff_id
-) REFERENCES tariff
-(
-    id
-),
-    CONSTRAINT fk_user FOREIGN KEY
-(
-    user_id
-) REFERENCES users
-(
-    id
-)
+    CONSTRAINT fk_tariff FOREIGN KEY (tariff_id) REFERENCES tariff(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
     );
 
-CREATE TABLE IF NOT EXISTS token_blacklist
-(
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    token
-    VARCHAR
-(
-    512
-) NOT NULL,
-    token_type VARCHAR
-(
-    50
-) NOT NULL,
+CREATE TABLE IF NOT EXISTS token_blacklist (
+    id SERIAL PRIMARY KEY,
+    token VARCHAR(512) NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
     user_id INTEGER NOT NULL,
     expiry_date TIMESTAMP NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY
-(
-    user_id
-) REFERENCES users
-(
-    id
-)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
     );
