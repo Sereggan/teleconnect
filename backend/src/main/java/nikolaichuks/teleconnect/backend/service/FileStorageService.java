6d3b9f78330e25fdfa1603f7f0d3f3ea056483fb
sereggan <sergei.ni@yandex.ru>
2024-10-29 01:10:57 +0100

    Small fixes

--- backend/src/main/java/nikolaichuks/teleconnect/backend/service/FileStorageService.java
+++
@@ -1,9 +0,0 @@
-package nikolaichuks.teleconnect.backend.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class FileStorageService {
-}
